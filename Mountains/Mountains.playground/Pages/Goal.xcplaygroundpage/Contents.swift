0d573d32c77a3965b682baf5e267bf94c623ec19
Student <student@macbook-pro-0419-knezevic.local>
2017-02-03 10:36:20 -0500

    great work

--- Mountains/Mountains.playground/Pages/Goal.xcplaygroundpage/Contents.swift
+++ Mountains/Mountains.playground/Pages/Goal.xcplaygroundpage/Contents.swift
@@ -22,6 +22,18 @@ canvas.drawLine(fromX: 100, fromY: 0, toX: 100, toY: 50)
 canvas.drawLine(fromX: 150, fromY: 0, toX: 150, toY: 100)
 canvas.drawLine(fromX: 200, fromY: 0, toX: 200, toY: 50)
 
+canvas.drawEllipse(centreX: 0, centreY: 300, width: 5, height: 5)
+canvas.drawEllipse(centreX: 0, centreY: 275, width: 5, height: 5)
+canvas.drawEllipse(centreX: 0, centreY: 250, width: 5, height: 5)
+canvas.drawEllipse(centreX: 0, centreY: 225, width: 5, height: 5)
+canvas.drawEllipse(centreX: 0, centreY: 200, width: 5, height: 5)
+canvas.drawEllipse(centreX: 0, centreY: 175, width: 5, height: 5)
+canvas.drawEllipse(centreX: 0, centreY: 150, width: 5, height: 5)
+canvas.drawEllipse(centreX: 0, centreY: 125, width: 5, height: 5)
+canvas.drawEllipse(centreX: 0, centreY: 100, width: 5, height: 5)
+canvas.drawEllipse(centreX: 0, centreY: 75, width: 5, height: 5)
+canvas.drawEllipse(centreX: 0, centreY: 50, width: 5, height: 5)
+canvas.drawEllipse(centreX: 0, centreY: 25, width: 5, height: 5)
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
 PlaygroundPage.current.liveView = canvas.imageView
